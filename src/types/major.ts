e0f50bd9c5e1436f7a4ad875b89c8b470ad63ab4
Evandro02 <evandrosilva@super.ufma.edu.br>
2024-08-17 09:09:56 -0400

    addd

--- src/types/major.ts
+++
@@ -1,6 +0,0 @@
-// src/types/major.ts
-
-import { Major } from '@prisma/client';
-
-export type CreateMajorDto = Pick<Major, 'name' | 'code' | 'description'>;
-export type UpdateMajorDto = Pick<Major, 'name' | 'code' | 'description'>;
